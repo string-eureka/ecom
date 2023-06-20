f4c4d0040e8b400dc72571dc9dcd4330afec2280
Gurekas Singh <45786301+string-eureka@users.noreply.github.com>
2023-06-20 23:08:01 +0530

    Dockerized, Added Gunicorn and Nginx

--- ecom/custom_azure.py
+++ ecom/custom_azure.py
@@ -5,12 +5,12 @@ environ.Env.read_env()
 
 class AzureMediaStorage(AzureStorage):
     account_name = env('AZURE_ACCOUNT_NAME')
-    account_key = "0DKGUiRJYN0yrfRH2qsb8uroDn/Za3mJrvwiGd9GEzuMHHaYy4R/DGhZllFOY1DIAVJBRSdWc9pf+ASt0c72XA=="
+    account_key = env('AZURE_KEY_1')
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = env('AZURE_ACCOUNT_NAME')
-    account_key = "i8tvNBJv0hI/D+CGOoiluihTAkVXIwKnFchtxh3ne3SmmmNKU1zFEUEBfptMoE2E9tcBGzHjceba+AStQNqMHQ=="
+    account_key = env('AZURE_KEY_2')
     azure_container = 'static'
     expiration_secs = None
