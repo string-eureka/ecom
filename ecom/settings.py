b93646d3fefdf72da2048892f8ed2165fb92bb1c
Gurekas Singh <45786301+string-eureka@users.noreply.github.com>
2023-06-21 16:23:30 +0530

    CSRF fixes

--- ecom/settings.py
+++ ecom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -189,3 +189,5 @@ SESSION_COOKIE_SECURE = True
 
 if 'WEBSITE_HOSTNAME' in os.environ: 
     from .azure import *
+CSRF_TRUSTED_ORIGINS = ['http://localhost:1337']
+CORS_ORIGIN_WHITELIST = ['http://localhost:1337']
